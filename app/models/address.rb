0c78876eafbec49609f64f72e13a2d7a041629cd
aefernandes <anjali.e.fernandes@gmail.com>
2018-07-13 08:52:38 -0800

    Updated to_s method, test passing

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,6 +1,13 @@
 class Address < ApplicationRecord
+  
+  # Prints out the address components as follows:
+  # house_number street_predirection street_name street_type street_postdirection unit_type unit_number, city, state, zip_5
   def to_s
-    # TODO: override the to_s method so that it prints out the address components as follows
-    # house_number street_predirection street_name street_type street_postdirection unit_type unit_number, city, state, zip_5
+    street_components = [
+      house_number, street_predirection, street_name, street_type, 
+      street_postdirection, unit_type, unit_number
+    ].compact.delete_if(&:empty?).join(' ')
+    return "#{street_components}, #{city}, #{state} #{zip_5}"
   end
+  
 end
