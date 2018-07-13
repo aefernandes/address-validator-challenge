1d0f2a4ab29519bee0c60d7779407d82f5652eef
aefernandes <anjali.e.fernandes@gmail.com>
2018-07-13 09:11:54 -0800

    Update address model and add tests

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,13 +1,25 @@
 class Address < ApplicationRecord
-  
+  NUMERICAL_REGEXP = /\d+/  # Verifies numericality without allowing + or -
+  validates :house_number, presence: true, format: { with: NUMERICAL_REGEXP} 
+  validates :street_name, :street_type, :city, :state, :zip_5, presence: true
+  validates :zip_5, length: { is: 5}, format: { with: NUMERICAL_REGEXP }
+
   # Prints out the address components as follows:
   # house_number street_predirection street_name street_type street_postdirection unit_type unit_number, city, state, zip_5
   def to_s
-    street_components = [
-      house_number, street_predirection, street_name, street_type, 
-      street_postdirection, unit_type, unit_number
-    ].compact.delete_if(&:empty?).join(' ')
-    return "#{street_components}, #{city}, #{state} #{zip_5}"
+    "#{street_address}, #{city}, #{state} #{zip_5}"
   end
-  
+
+  # Joins street address components with a space and removes blanks
+  def street_address
+    [
+      house_number, 
+      street_predirection, 
+      street_name, 
+      street_type, 
+      street_postdirection, 
+      unit_type, 
+      unit_number
+    ].compact.delete_if(&:empty?).join(' ')
+  end
 end
--- spec/models/address_spec.rb
+++ spec/models/address_spec.rb
@@ -36,5 +36,88 @@ RSpec.describe Address, :type => :model do
         expect(address.to_s).to eq('129 W 81st St Apt 5A, New York, NY 10024')
       end
     end
+
+    describe '#street_address' do
+      context 'Given address without predirection' do
+        let(:address) { create(:address_ny, street_predirection: nil) }
+        it 'prints out the address components without an extra space' do
+          expect(address.street_address).to eq('129 81st St Apt 5A')
+        end
+      end
+
+      context 'Given address without postdirection' do
+        let(:address) { create(:address_ny, street_postdirection: nil) }
+        it 'prints out the address components without an extra space' do
+          expect(address.street_address).to eq('129 W 81st St Apt 5A')
+        end
+      end
+
+      context 'Given address without unit type' do
+        let(:address) { create(:address_ny, unit_type: nil) }
+        it 'prints out the address components without an extra space' do
+          expect(address.street_address).to eq('129 W 81st St 5A')
+        end
+      end
+
+      context 'Given address without unit number' do
+        let(:address) { create(:address_ny, unit_number: nil) }
+        it 'prints out the address components without an extra space' do
+          expect(address.street_address).to eq('129 W 81st St Apt')
+        end
+      end
+    end
+
+    subject {
+      described_class.new(
+        house_number: 1600,
+        street_name: 'Pennsylvania',
+        street_type: 'Avenue',
+        street_postdirection: 'NW',
+        city: 'Washington',
+        state: 'DC',
+        zip_5: 20500
+      )
+    }
+
+    it 'is not valid without a house number' do
+      subject.house_number = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without a street name' do
+      subject.street_name = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without a city' do
+      subject.city = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without a state' do
+      subject.state = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without a zip code' do
+      subject.zip_5 = nil
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without a numeric house number' do
+      subject.house_number = "abcde"
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid without a numeric zip code' do
+      subject.zip_5 = "abcde"
+      expect(subject).not_to be_valid
+    end
+
+    it 'is not valid with a zip code less than length 5' do
+      subject.zip_5 = "1001"
+      expect(subject).not_to be_valid
+    end
+
   end
 end
